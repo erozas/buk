d928c18461cb348790ab543b6df13bcf885e7d15
Exequiel Rozas <exequiel98@gmail.com>
2025-03-21 19:46:47 -0400

    Order by 'published_at' descending for the index and add '@popular_articles' to the show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.published.order(published_at: :desc)
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.friendly.find(params[:id])
+    @popular_articles = Article.published.order(published_at: :desc).limit(3)
   end
 end
