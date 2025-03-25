aaf50f21b19726fd4e1dc977c402c2e1b36a06b3
Exequiel Rozas <exequiel98@gmail.com>
2025-03-25 18:21:17 -0400

    Apply schema

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+  before_action :set_schema, only: [:show]
+
   def index
     @articles = Article.published.order(published_at: :desc)
   end
@@ -6,5 +9,29 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.friendly.find(params[:id])
     @popular_articles = Article.published.order(published_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_schema
+    organization = SchemaDotOrg::Organization.new(
+      name: "Buk",
+      url: "https://buk.com",
+      email: "hello@buk.com",
+      logo: "https://buk.com/logo.png",
+      telephone: "+1234567890"
+    )
+
+    article = SchemaDotOrg::Article.new(
+      name: @article.title,
+      url: article_url(@article),
+      description: @article.excerpt,
+      article_body: @article.content,
+      date_published: @article.published_at,
+      publisher: organization,
+      image: @article.image,
+    )
+
+    @schema = SchemaDotOrg::GraphContainer.new([organization, article])
   end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,6 @@
 class StaticController < ApplicationController
+  before_action :set_schema, only: [:home]
+
   def home
     @books = Book.all
   end
@@ -12,5 +14,33 @@ class StaticController < ApplicationController
     respond_to do |format|
       format.xml
     end
+  end
+
+  private
+
+  def set_schema
+    organization = SchemaDotOrg::Organization.new(
+      name: "Buk",
+      url: "https://buk.com",
+      email: "hello@buk.com",
+      logo: "https://buk.com/logo.png",
+      telephone: "+1234567890"
+    )
+
+    web_site = SchemaDotOrg::WebSite.new(
+      url: "https://buk.com",
+      name: "Buk",
+      about: "Buk is a book sharing platform where users can gather and share their favorite books with each other.",
+      publisher: organization
+    )
+
+    web_page = SchemaDotOrg::WebPage.new(
+      url: "https://buk.com",
+      name: "Buk",
+      description: "Buk is a platform for reading books online.",
+      in_language: "en-US",
+    )
+
+    @schema = SchemaDotOrg::GraphContainer.new([organization, web_site, web_page])
   end
 end
