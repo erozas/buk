3f63012b2759465a7ce47862c8a5d6c1cba4ee10
Exequiel Rozas <exequiel98@gmail.com>
2025-03-25 18:21:08 -0400

    Sorting and filtering

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,26 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    query = Book.all
+  
+    # Apply category filtering
+    if params[:genre].present?
+      query = query.joins(:genres).where(genres: { slug: params[:genre] })
+    end
+  
+    # Apply sorting
+    case params[:sort]
+    when "title_asc"
+      query = query.order(title: :asc)
+    when "title_desc"
+      query = query.order(title: :desc)
+    else # "recent" is default
+      query = query.order(created_at: :desc)
+    end
+  
+    @pagy, @books = pagy(query, items: 12)
+  
+    # Only render the books part if it's a Turbo Frame request
+    render partial: "books/book_grid", locals: { books: @books, pagy: @pagy } if turbo_frame_request?
   end
 
   def show
