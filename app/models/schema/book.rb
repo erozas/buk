7d22f924d008247974e8beda2499c804e3fbf729
Exequiel Rozas <exequiel98@gmail.com>
2025-03-25 18:19:57 -0400

    Book and BreadcrumbList models

--- app/models/schema/book.rb
+++
@@ -1,5 +0,0 @@
-module Schema
-  class Book < SchemaDotOrg::SchemaType
-    validated_attr :name, type: String
-  end
-end
--- app/models/schema/breadcrumb_list.rb
+++
@@ -1,5 +0,0 @@
-module Schema
-  class Breadcrumb < SchemaDotOrg::SchemaType
-    validated_attr :name, type: String
-  end
-end
